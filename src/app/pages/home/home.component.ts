5b820ac1a14cb2c6b39e2eee3ce6461b9b51c38f
thamytolomei <thamytolomei@gmail.com>
2025-06-10 17:22:46 -0300

    fixes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  constructor(
+    public router: Router,
+  ) { }
+
+  redirectTo(page:string){
+    this.router.navigateByUrl(page);
+  }
 }
